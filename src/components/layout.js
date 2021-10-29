ce08b6eaccffb75664c56a48ab7743e2c0b65af4
Chris Odegard <c.odegard@gmail.com>
2021-10-29 14:59:40 -0400

    Added Fluent UI theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,33 @@
 import * as React from "react";
 import { useStaticQuery, graphql, Link } from "gatsby";
+import { Stack, createTheme, ThemeProvider } from "@fluentui/react";
+
+const myTheme = createTheme({
+  palette: {
+    themePrimary: '#2334cc',
+    themeLighterAlt: '#010208',
+    themeLighter: '#060821',
+    themeLight: '#0a0f3d',
+    themeTertiary: '#151f7a',
+    themeSecondary: '#1f2db4',
+    themeDarkAlt: '#3545d1',
+    themeDark: '#505dd8',
+    themeDarker: '#7984e2',
+    neutralLighterAlt: '#f8f8f8',
+    neutralLighter: '#f4f4f4',
+    neutralLight: '#eaeaea',
+    neutralQuaternaryAlt: '#dadada',
+    neutralQuaternary: '#d0d0d0',
+    neutralTertiaryAlt: '#c8c8c8',
+    neutralTertiary: '#a19f9d',
+    neutralSecondary: '#605e5c',
+    neutralPrimaryAlt: '#3b3a39',
+    neutralPrimary: '#323130',
+    neutralDark: '#201f1e',
+    black: '#000000',
+    white: '#ffffff',
+  }
+});
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`;
@@ -34,7 +62,8 @@ const Layout = ({ location, title, children }) => {
   }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
+    <ThemeProvider theme={myTheme}>
+    <Stack className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
@@ -44,7 +73,8 @@ const Layout = ({ location, title, children }) => {
         {` `}
         {data.site.buildTime}
       </footer>
-    </div>
+    </Stack>
+    </ThemeProvider>
   );
 };
 
