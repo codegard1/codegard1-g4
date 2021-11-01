f2c5f0aa315cbddad9199830a8826c3f7b41f17f
Chris Odegard <c.odegard@gmail.com>
2021-11-01 17:54:03 -0400

    update bio component

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,13 +1,8 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
+
+import { Stack } from '@fluentui/react';
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -37,7 +32,7 @@ const Bio = () => {
   const social = data.site.siteMetadata?.social;
 
   return (
-    <div className="bio">
+    <Stack horizontal>
       <StaticImage
         className="bio-avatar"
         layout="fixed"
@@ -50,14 +45,14 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.keybase || ``}`}>
+          Written by <strong>{author.name}</strong>
+          <br />
+          <a href={`${social?.keybase || ``}`}>
             You should follow them on Keybase
           </a>
         </p>
       )}
-    </div>
+    </Stack>
   );
 };
 
