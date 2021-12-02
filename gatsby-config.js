fb6a1ea56a243b8d8b1bb16f571b23fd5a4ae826
Chris Odegard <c.odegard@gmail.com>
2021-12-02 16:26:56 -0500

    remove images

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,13 +49,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `gallery`,
-        path: `${__dirname}/content/gallery`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
